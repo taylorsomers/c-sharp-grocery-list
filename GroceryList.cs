067ccba92c350a0f1964069993a26163c1d4754d
Taylor Somers <tsomers@yandex.com>
2020-07-13 10:56:23 -0400

    Add foreach loop to displate grocery items in console

--- GroceryList.cs
+++ GroceryList.cs
@@ -5,5 +5,12 @@ class GroceryList
   static void Main()
   {
     string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
+
+    Console.WriteLine("My grocery list:");
+
+    foreach (string groceryItem in myGroceryList)
+    {
+      Console.WriteLine(groceryItem);
+    }
   }
 }
