b1552ae8b67c254fd2fa194b0d274e60761e3a95
Taylor Somers <tsomers@yandex.com>
2020-07-13 10:59:01 -0400

    Add grocery list prices summation logic

--- GroceryList.cs
+++ GroceryList.cs
@@ -5,12 +5,20 @@ class GroceryList
   static void Main()
   {
     string[] myGroceryList = {"eggs", "milk", "bread", "bananas", "cereal", "rice", "yogurt"};
+    int[] groceryListPrices = {3, 6, 4, 2, 4, 4};
 
     Console.WriteLine("My grocery list:");
-
     foreach (string groceryItem in myGroceryList)
     {
       Console.WriteLine(groceryItem);
     }
+
+    int total = 0;
+    foreach (int price in groceryListPrices)
+    {
+      total = total + price;
+    }
+
+    Console.WriteLine("Your total for this shopping trip will be$" + total);
   }
 }
